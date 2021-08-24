09e41ca8c003ea4156814d043f13082b5c670d24
evenyoung <yiyun@igola.com>
2021-08-24 13:23:29 -0400

    [hw3]dnspoison.go: remove useless comments

--- hw3/poison/dnspoison.go
+++ hw3/poison/dnspoison.go
@@ -1,11 +1,3 @@
-// Copyright 2012 Google, Inc. All rights reserved.
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file in the root of the source
-// tree.
-
-// The pcaplay binary load an offline capture (pcap file) and replay
-// it on the select interface, with an emphasis on packet timing
 package poison
 
 import (
